9399c943a464a30ef8d5f85f83e5ed1aac61adba
mehdi <zahranemehdi@gmail.com>
2016-04-02 17:30:30 +0100

    Update database.php

--- config/database.php
+++ config/database.php
@@ -54,10 +54,10 @@ return [
 
         'mysql' => [
             'driver'    => 'mysql',
-            'host'      => env('DB_HOST', 'localhost'),
-            'database'  => env('DB_DATABASE', 'forge'),
-            'username'  => env('DB_USERNAME', 'forge'),
-            'password'  => env('DB_PASSWORD', ''),
+            'host'      => env('DB_HOST', 'mysql://$OPENSHIFT_MYSQL_DB_HOST:$OPENSHIFT_MYSQL_DB_PORT/'),
+            'database'  => env('DB_DATABASE', 'test'),
+            'username'  => env('DB_USERNAME', 'admin39dv5yn'),
+            'password'  => env('DB_PASSWORD', 'FQehMMw1rTjh'),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
